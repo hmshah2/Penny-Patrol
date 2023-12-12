1019360adba54dab4da1b47706ad9ae8afba8dca
meghna2409 <meghna.jayaraj24@gmail.com>
2023-12-12 03:51:19 -0600

    Completed header and routes

---
+++ frontend/src/components/EmptyPage/EmptyPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EmptyPage = ({ title }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+};
+
+export default EmptyPage;
---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './Header.module.css';
+
+const Header = ({ activeNav, setActiveNav }) => {
+  return (
+    <div className={styles.fullHeader}>
+      <header className={styles.appHeader}>
+        Penny Patrol
+      </header>
+      <nav className={styles.appNav}>
+        <NavLink to="/purchases" className={({ isActive }) => isActive ? styles.navButtonActive : styles.navButton} onClick={() => setActiveNav('Purchases')}>Purchases</NavLink>
+        <NavLink to="/spending-log" className={({ isActive }) => isActive ? styles.navButtonActive : styles.navButton} onClick={() => setActiveNav('Spending Log')}>Spending Log</NavLink>
+        <NavLink to="/pie-chart" className={({ isActive }) => isActive ? styles.navButtonActive : styles.navButton} onClick={() => setActiveNav('Financial Analysis')}>Financial Analysis</NavLink>
+        <div className={styles.userIcon}>👤</div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
